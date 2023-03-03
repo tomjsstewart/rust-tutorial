7b36f6518e5e0d836f11a7fd28def83557e444a0
Tom Stewart <tstewart@photonicscience.com>
2023-03-03 22:14:08 +0000

    Comparing input to output

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -6,7 +7,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is {secret_number}");
-
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -16,4 +16,12 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guesssed {guess}");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
