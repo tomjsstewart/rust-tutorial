b0af8a69cb1cd7121aadb2300036a2bed3d7400e
Tom Stewart <tstewart@photonicscience.com>
2023-03-04 18:22:43 +0000

    Constants

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,4 +4,9 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is {x}");
+
+    // constants use const and require a type annotation, expression is evaluated at compile time
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("Constant: {THREE_HOURS_IN_SECONDS}");
+
 }
