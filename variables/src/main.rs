13dc407404bb63765803e9e0d75e092c1d61d808
Tom Stewart <tstewart@photonicscience.com>
2023-03-04 18:32:14 +0000

    Shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -9,4 +9,14 @@ fn main() {
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("Constant: {THREE_HOURS_IN_SECONDS}");
 
+    // shadowing, inside the inner scope the varible x is overshadowed and returns to the previous value outside the scope
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+    println!("The value of x is {x}");
+
+    // by using let we can shadow the spaces variable with 2 different types
+    let spaces = "   ";
+    let spaces = spaces.len();
 }
