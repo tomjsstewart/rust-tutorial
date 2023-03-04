d4e814b5a1e5ea8a5c65365a6de10e9e942a0a36
Tom Stewart <tstewart@photonicscience.com>
2023-03-04 21:49:32 +0000

    if statements

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    // if number {} is not valid as rust will not cast to boolean automatically
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    // if is an expression, we can use it on the right side of a let statement
+    let y = if false {6} else {-1};
 }
