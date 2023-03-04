8cc54b67d882b2abc713871d38fa7036d3ae3bc3
Tom Stewart <tstewart@photonicscience.com>
2023-03-04 17:56:37 +0000

    Handle invalid input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -17,9 +17,12 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        println!("You guesssed {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guesssed {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
