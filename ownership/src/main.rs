e37b20406a92baaa62fca3b3d7724ea022e48d68
Tom Stewart <tstewart@photonicscience.com>
2023-03-05 23:55:27 +0000

    Function ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -8,6 +8,8 @@ fn main() {
     // borrow_error();
 
     cloning();
+
+    function_ownership();
 }
 
 // fn scope(){            // s is not valid here, it’s not yet declared
@@ -36,3 +38,34 @@ fn cloning() {
     // let y = x;
     // x is still valid here as it is only assigned on the stack
 }
+
+fn function_ownership() {
+    let s = String::from("hello"); // s comes into scope
+
+    takes_ownership(s); // s's value moves into the function...
+                        // ... and so is no longer valid here
+
+    let x = 5; // x comes into scope
+
+    makes_copy(x); // x would move into the function,
+                   // but i32 is Copy, so it's okay to still
+                   // use x afterward
+
+    println!("x is still in scope here {x}");
+
+    // Cannot do this as s's owner is `takes_ownership` and that scope has ended
+    // so s has been dropped
+    // println!("I cannot print s: {s}");
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing
+  // special happens.
+
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
+    println!("I now own some_string: {}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
+    println!("I have a copy of some_integer: {}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
