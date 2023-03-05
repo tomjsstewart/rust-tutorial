5154e2f6ffd70132da0e221d664180453bb933b3
Tom Stewart <tstewart@photonicscience.com>
2023-03-05 23:59:17 +0000

    returning values to transfer ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -10,6 +10,8 @@ fn main() {
     cloning();
 
     function_ownership();
+
+    return_values();
 }
 
 // fn scope(){            // s is not valid here, it’s not yet declared
@@ -69,3 +71,34 @@ fn makes_copy(some_integer: i32) {
     // some_integer comes into scope
     println!("I have a copy of some_integer: {}", some_integer);
 } // Here, some_integer goes out of scope. Nothing special happens.
+
+
+fn return_values() {
+    let s1 = gives_ownership();         // gives_ownership moves its return
+                                        // value into s1
+
+    let s2 = String::from("hello");     // s2 comes into scope
+
+    let s3 = takes_and_gives_back(s2);  // s2 is moved into
+                                        // takes_and_gives_back, which also
+                                        // moves its return value into s3
+} // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
+  // happens. s1 goes out of scope and is dropped.
+
+fn gives_ownership() -> String {             // gives_ownership will move its
+                                             // return value into the function
+                                             // that calls it
+
+    let some_string = String::from("yours"); // some_string comes into scope
+
+    some_string                              // some_string is returned and
+                                             // moves out to the calling
+                                             // function
+}
+
+// This function takes a String and returns one
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
+                                                      // scope
+
+    a_string  // a_string is returned and moves out to the calling function
+}
