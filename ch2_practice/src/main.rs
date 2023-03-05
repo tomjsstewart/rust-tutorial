6ab9de39d66c957a95be8f98e1b69f36a9e30b7e
Tom Stewart <tstewart@photonicscience.com>
2023-03-05 20:14:35 +0000

    Lyrics to 12 days of christmas

--- ch2_practice/src/main.rs
+++ ch2_practice/src/main.rs
@@ -12,6 +12,9 @@ fn main() {
             fib_recur(i)
         );
     }
+
+    println!("Lyrics to the 12 days of christmas are:");
+    days_of_christmas();
 }
 
 fn fahrenheit_to_celsius(f: f64) -> f64 {
@@ -50,4 +53,49 @@ fn fib_recur(n: u32) -> u64 {
     }
 
     return fib_recur(n - 1) + fib_recur(n - 2);
+}
+
+fn days_of_christmas() {
+    for day in 1..=12 {
+        // logic for correct number here is too much effort
+        println!("On the {day}th day of Christmas, my true love sent to me");
+
+        if day >= 12 {
+            println!("Twelve drummers drumming");
+        }
+        if day >= 11 {
+            println!("Eleven pipers piping");
+        }
+        if day >= 10 {
+            println!("Ten lords a-leaping");
+        }
+        if day >= 9 {
+            println!("Nine ladies dancing");
+        }
+        if day >= 8 {
+            println!("Eight maids a-milking");
+        }
+        if day >= 7 {
+            println!("Seven swans a-swimming");
+        }
+        if day >= 6 {
+            println!("Six geese a-laying");
+        }
+        if day >= 5 {
+            println!("Five golden rings");
+        }
+        if day >= 4 {
+            println!("Four calling birds");
+        }
+        if day >= 3 {
+            println!("Three french hens");
+        }
+        if day >= 2 {
+            println!("Two turtle doves, and");
+        }
+        if day >= 1 {
+            println!("A partridge in a pear tree");
+        }
+        println!("");
+    }
 }
