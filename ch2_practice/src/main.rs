3cc866f52b71e15ed16e88802a53706a642e45e4
Tom Stewart <tstewart@photonicscience.com>
2023-03-05 20:02:59 +0000

    Fahrenheit and Celsius converter

--- ch2_practice/src/main.rs
+++ ch2_practice/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    println!("400f in c = {}", fahrenheit_to_celsius(400.0));
+    println!("25c in f = {}", celsius_to_fahrenheit(25.0));
+}
+
+fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - 32.0) / 1.8
+}
+
+fn celsius_to_fahrenheit(c: f64) -> f64 {
+    (c * 1.8) + 32.0
 }
