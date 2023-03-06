96712fa97f8a729e28273f26f4b7051e25ead2c5
Tom Stewart <tstewart@photonicscience.com>
2023-03-06 00:23:21 +0000

    references

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -12,6 +12,10 @@ fn main() {
     function_ownership();
 
     return_values();
+
+    references();
+
+    mutable_reference();
 }
 
 // fn scope(){            // s is not valid here, it’s not yet declared
@@ -72,33 +76,93 @@ fn makes_copy(some_integer: i32) {
     println!("I have a copy of some_integer: {}", some_integer);
 } // Here, some_integer goes out of scope. Nothing special happens.
 
-
 fn return_values() {
-    let s1 = gives_ownership();         // gives_ownership moves its return
-                                        // value into s1
+    let s1 = gives_ownership(); // gives_ownership moves its return
+                                // value into s1
 
-    let s2 = String::from("hello");     // s2 comes into scope
+    let s2 = String::from("hello"); // s2 comes into scope
 
-    let s3 = takes_and_gives_back(s2);  // s2 is moved into
-                                        // takes_and_gives_back, which also
-                                        // moves its return value into s3
+    let s3 = takes_and_gives_back(s2); // s2 is moved into
+                                       // takes_and_gives_back, which also
+                                       // moves its return value into s3
 } // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
   // happens. s1 goes out of scope and is dropped.
 
-fn gives_ownership() -> String {             // gives_ownership will move its
-                                             // return value into the function
-                                             // that calls it
+fn gives_ownership() -> String {
+    // gives_ownership will move its
+    // return value into the function
+    // that calls it
 
     let some_string = String::from("yours"); // some_string comes into scope
 
-    some_string                              // some_string is returned and
-                                             // moves out to the calling
-                                             // function
+    some_string // some_string is returned and
+                // moves out to the calling
+                // function
 }
 
 // This function takes a String and returns one
-fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
-                                                      // scope
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string comes into
+    // scope
 
-    a_string  // a_string is returned and moves out to the calling function
+    a_string // a_string is returned and moves out to the calling function
+}
+
+fn references() {
+    let s1 = String::from("hello");
+
+    // Pass a reference to calculate_length so that it is not dropped
+    // This is called borrowing
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len)
+}
+
+fn calculate_length(s: &String) -> usize {
+    // s is a reference to a String
+    return s.len();
+} // Here, s goes out of scope. But because it does not have ownership of what
+  // it refers to, it is not dropped.
+
+
+// Immutable borrowing
+//   fn immutable_borrowing() {
+//     let s = String::from("hello");
+
+//     change(&s);
+// }
+
+// Borrowing is immutable and we cannot modify something we have a reference to
+// fn change(some_string: &String) {
+//     some_string.push_str(", world");
+// }
+
+
+// Mutable references
+fn mutable_reference(){
+    let mut s1 = String::from("hello");
+
+    // If there is a mutable reference to s1, no other references to s1, mutable or otherwise, can exist
+    // Multiple immutable references is fine
+    change(&mut s1);
+
+    println!("s1 is now: {s1}");
+
+    mutable_after_immutable();
+}
+
+fn change(s: &mut String){
+    s.push_str(" world!");
+}
+
+fn mutable_after_immutable() {
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
