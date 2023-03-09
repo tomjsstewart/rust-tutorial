37c80342abdbcd6bd907fcc9b3b3502298359893
Tom Stewart <tstewart@photonicscience.com>
2023-03-09 23:41:53 +0000

    area naive

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_naive(width1, height1)
+    );
+}
+
+fn area_naive(width: u32, height:u32) -> u32 {
+    width*height
 }
