2ebc09b06f99f4285e01da1fb294f995038d7b9b
Tom Stewart <tstewart@photonicscience.com>
2023-03-09 23:44:01 +0000

    area with tuple

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -3,11 +3,20 @@ fn main() {
     let height1 = 50;
 
     println!(
-        "The area of the rectangle is {} square pixels.",
+        "The area of the rectangle is {} square pixels (naive).",
         area_naive(width1, height1)
+    );
+
+    println!(
+        "The area of the rectangle is {} square pixels (tuple).",
+        area_tuple((width1, height1))
     );
 }
 
 fn area_naive(width: u32, height:u32) -> u32 {
     width*height
+}
+
+fn area_tuple(dimentions: (u32, u32)) -> u32 {
+    dimentions.0 * dimentions.1
 }
