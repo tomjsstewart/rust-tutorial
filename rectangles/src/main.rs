3346197758cdb4bfc6e80cabf9d76a7504cab74a
Tom Stewart <tstewart@photonicscience.com>
2023-03-10 00:42:38 +0000

    :Methods with more parameters

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -15,8 +15,11 @@ impl Rectangle {
     fn width(&self) -> bool {
         self.width > 0
     }
-}
 
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        (self.width > other.width) && (self.height > other.height)
+    }
+}
 
 // Can have more than one impl block
 impl Rectangle {
@@ -72,6 +75,23 @@ fn main() {
     if rect.width() {
         println!("The rectangle has a nonzero width; it is {}", rect.width);
     }
+
+    // Methods with parameters
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 fn area_naive(width: u32, height: u32) -> u32 {
