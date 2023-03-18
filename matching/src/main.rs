82cab1c1407b83f794aa75e88712eaf6bb8423e7
Tom Stewart <tstewart@photonicscience.com>
2023-03-18 20:13:14 +0000

    if let

--- matching/src/main.rs
+++ matching/src/main.rs
@@ -21,6 +21,37 @@ fn add_one(x: Option<i32>) -> Option<i32> {
     }
 }
 
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {
+    println!("{num_spaces}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let random_roll = 77; // A very random number
+    match random_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other), // Other is a variable and can be named anything we want
+                                     // Can use _ => ... To match all other values and throw them away
+    };
+
+    if_let();
+}
+
+fn if_let() {
+    let config_max = Some(3u8);
+    // if let provides a shorthand to match a single value with an optional else statement to handle any other values
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    } else {
+        println!("The maximum was not provided")
+    }
+
+    // This is equivalent to:
+    // let config_max = Some(3u8);
+    // match config_max {
+    //     Some(max) => println!("The maximum is configured to be {}", max),
+    //     _ => (),
+    // }
 }
