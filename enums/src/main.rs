a89db6cc05c316988049b8d3093a19a6173cf0b6
Tom Stewart <tstewart@photonicscience.com>
2023-03-18 18:22:07 +0000

    Basic enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,10 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Enum variants are namespaced under the identifier
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
 }
