a418aa2fbed1391460ee2bb0194887dad4353720
Tom Stewart <tstewart@photonicscience.com>
2023-03-18 19:51:04 +0000

    basic matching

--- matching/src/main.rs
+++ matching/src/main.rs
@@ -1,3 +1,26 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn add_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(x) => Some(x + 1),
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
