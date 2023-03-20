bb4e7b04184fa3c7804c79e27557f9a68647c5ec
Tom Stewart <tstewart@photonicscience.com>
2023-03-20 21:01:33 +0000

    super

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,3 +1,14 @@
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
