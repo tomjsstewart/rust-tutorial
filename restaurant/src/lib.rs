7261541e068e4b6890c79eea77c95c02a5f3ad16
Tom Stewart <tstewart@photonicscience.com>
2023-03-20 21:11:10 +0000

    Public structs

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -7,6 +7,21 @@ mod back_of_house {
     }
 
     fn cook_order() {}
+
+    pub struct Breakfast {
+        // struct fields are private by default
+        pub toast: String,      // This struct field is public
+        seasonal_fruit: String, // This field is not
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("Peaches"),
+            }
+        }
+    }
 }
 
 mod front_of_house {
@@ -31,4 +46,14 @@ pub fn eat_at_restaurant() {
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
 }
