2d22b4eee3549af7b038e0514022f74d0c53c07e
Tom Stewart <tstewart@photonicscience.com>
2023-04-10 18:37:02 +0100

    move modules to different files

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -30,21 +30,7 @@ mod back_of_house {
     }
 }
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 pub fn eat_at_restaurant() {
     // Absolute path
