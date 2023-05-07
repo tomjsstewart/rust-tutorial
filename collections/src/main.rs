cc4ca414a706a3964bfe7c6c430d189d9814a61a
Tom Stewart <tstewart@photonicscience.com>
2023-05-07 18:49:04 +0100

    HashMap

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,7 +1,11 @@
+use std::collections::HashMap;
+
 fn main() {
     vector();
 
     string();
+
+    hash_map();
 }
 
 enum SpreadSheetCell {
@@ -93,4 +97,39 @@ fn string() {
     for c in "Зд".bytes() {
         println!("{c}"); // each utf-8 character is 2 bytes
     }
+}
+
+fn hash_map() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    // Get a value with its key
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    for (key, value) in scores {
+        println!("{key}: {value}");
+    }
+
+    let mut scores2 = HashMap::new();
+    scores2.insert(String::from("Blue"), 10);
+
+    scores2.entry(String::from("Yellow")).or_insert(50); // Inserts Yellow: 50
+    scores2.entry(String::from("Blue")).or_insert(50); // Does not change Blue
+
+    println!("{:?}", scores2);
+    println!("{:?}", scores2.entry(String::from("pink")));
+    println!("{:?}", scores2.entry(String::from("Blue")));
+
+    let text = "hello world wonderful world";
+    let mut text_map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        // or_insert returns a mutable reference to the entry for `word`
+        let count = text_map.entry(word).or_insert(0);
+        *count += 1; // Increment the count
+    }
+
+    println!("{:?}", text_map);
 }
