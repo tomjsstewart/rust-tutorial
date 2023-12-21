9aa76c5219aa6f419ec7a1eac532106161d96c17
Tom Stewart <tstewart@photonicscience.com>
2023-12-21 18:40:52 +0000

    panicing code

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,3 +1,18 @@
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    // crash_and_burn();
+    // panic_again();
+}
+
+
+fn crash_and_burn() {
+    // Only call to panic code!
+    panic!("crash and burn");
+}
+
+fn panic_again() {
+    let v = vec![1,2,3];
+
+    v[99];  // Illegal access
 }
