998dd0016ebac0c1bdac9aeb8356f6ecf318b436
Tom Stewart <tstewart@photonicscience.com>
2023-12-22 14:53:13 +0000

    Generic structs

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -36,6 +36,16 @@ fn largest_char(list: &[char]) -> &char {
 
 //     largest
 // }
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+// Point where x and y have different generic types
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
@@ -47,4 +57,10 @@ fn main() {
 
     let result = largest_char(&char_list);
     println!("The largest char is {}", result);
+
+    // Generic Struct
+    let float_point = Point { x: 5.2, y: 7.9 };
+    let int_point = Point { x: 8, y: 6 };
+
+    // let wont_work = Point { x: 5, y: 4.0 };  // This will not compile as x and y are not the same types
 }
