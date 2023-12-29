b90045a67a4e1f7f0491671451cdf80fa2e7ba44
Tom Stewart <tstewart@photonicscience.com>
2023-12-29 00:47:13 +0000

    Rectangle code with test

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,5 +1,13 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +15,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle{width: 8, height:7};
+        let smaller = Rectangle{width: 5, height:1};
+
+        assert!(larger.can_hold(&smaller));
     }
 }
