ec39da3443459cd5cdc9b8af5aa09513ba424da3
Tom Stewart <tstewart@photonicscience.com>
2023-12-30 19:17:03 +0000

    Other assert macros

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -10,15 +10,44 @@ impl Rectangle {
     }
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn larger_can_hold_smaller() {
-        let larger = Rectangle{width: 8, height:7};
-        let smaller = Rectangle{width: 5, height:1};
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
 
         assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(add_two(2), 4);
     }
 }
