53653fdd1643e4c53246637411fbbda980110a21
Tom Stewart <tstewart@photonicscience.com>
2023-12-30 19:43:24 +0000

    Result in tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -56,4 +56,13 @@ mod tests {
     fn this_panics() {
         panic!("HEEEELLLPPPP!!");
     }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
 }
---
+++ adder/tests/integration_tests.rs
@@ -0,0 +1,6 @@
+use adder::{self, add_two};
+
+#[test]
+fn it_adds_two() {
+    assert_eq!(add_two(3), 5);
+}
