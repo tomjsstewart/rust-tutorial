60d4b7a2fcda49866f1387d7c59a7864ae9d8229
Tom Stewart <tstewart@photonicscience.com>
2023-12-31 14:16:53 +0000

    Organising common test code

---
+++ adder/tests/common/mod.rs
@@ -0,0 +1,5 @@
+// Use submodle and mod.rs structure to avoid common crate being run by cargo test
+
+pub fn setup() {
+    // setup code specific to your library's tests would go here
+}
--- adder/tests/integration_tests.rs
+++ adder/tests/integration_tests.rs
@@ -1,6 +1,9 @@
 use adder::{self, add_two};
 
+mod common;
+
 #[test]
 fn it_adds_two() {
+    common::setup(); // Run the common setup
     assert_eq!(add_two(3), 5);
 }
