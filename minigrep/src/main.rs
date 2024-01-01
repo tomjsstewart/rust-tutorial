0a1663f0b56f004eb01bdb979e3b94463587c845
Tom Stewart <tstewart@photonicscience.com>
2024-01-01 13:03:06 +0000

    Refactor to extract arg parsing to function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,8 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for {query}");
     println!("In file {file_path}");
@@ -13,4 +12,11 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should be able to read a file");
 
     println!("With text:\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
